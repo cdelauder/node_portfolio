c1a4f18d56af364f942e55de16845b2efd6fd66f
cdelauder <cdelauder@gmail.com>
2014-09-23 18:44:56 -0700

    the structure is there, just need to fix styling, make sure all of my client js is working, and get the mailer up and running

--- index.js
+++ index.js
@@ -16,22 +16,45 @@ server.get('/contact', function (req, res) {
   res.render('contact')
 })
 
+server.get('/home', function (req, res) {
+  res.render('home')
+})
+
 server.get('/email', function (req, res) {
-  //lot more to do here
+  var gmailPW = require('/.env')
+  console.log($PW)
   res.render('email')
+})
+
+server.get('/resume', function (req, res) {
+  res.render('resume')
 })
 
 server.get('/stylesheet.css', function (req, res) {
   res.sendFile(__dirname + '/public/css/stylesheet.css')
 })
 
+server.get('/chrisdelauder.pdf', function (req, res) {
+  res.sendFile(__dirname + '/public/files/chrisdelauder.pdf')
+})
+
 server.get('/client.js', function (req, res) {
-  console.log('wtf')
   res.sendFile(__dirname + '/public/js/client.js')
 })
 
 server.get('/:image', function (req, res) {
   res.sendFile(__dirname + '/public/images/' + req.params.image)
+})
+
+server.post('/email', function (req, res) {
+  var mailer = require('nodemailer')
+  var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'cdelauder@gmail.com',
+      pass: $PW
+    }
+  });
 })
 
 server.listen(3000)
--- public/js/client.js
+++ public/js/client.js
@@ -2,6 +2,7 @@ $(document).ready(bindListeners)
 
 function bindListeners () {
   $('.content-links').one('click', gethtml)
+  $('.email-link').one('click', getOverlayContent)
 }
 
 function gethtml (e) {
@@ -13,6 +14,53 @@ function gethtml (e) {
 }
 
 function addToPage (e) {
-  $('.main-content').empty()
-  $('.main-content').append(e)
+  $('.main-content').empty().append(e)
+}
+
+function getOverlayContent (e) {
+  e.preventDefault()
+  var ajax = $.ajax({
+    url: e.target.href
+
+  }).done(overlayContent)
+}
+
+
+var overlayContent = function(e) {
+  $('html').append('<div class="shadow"></div>')
+  $('.main-content').prepend(e)
+  $('.email-textfield').one('click', emailSend)
+  $('.close').one('click', cancel)
+}
+
+var emailSend = function(e) {
+  e.preventDefault()
+  var ajax = $.ajax({
+    url: e.target.href
+
+  })
+  $('.email-textfield').append('Sending...')
+  ajax.done(emailSent)
+  ajax.error(emailError)
+}
+
+var emailSent = function() {
+  toggleEmailForm()
+  $('.email-status').text('Email sent')
+}
+
+var emailError = function() {
+  toggleEmailForm()
+  $('.email-status').text('Error, please try again')
+}
+
+var cancel = function() {
+  toggleEmailForm()
+}
+
+var toggleEmailForm = function() {
+  $('.shadow').remove()
+  $('.email-form').remove()
+  $('.email').one('click', emailSend)
+  $('.email-link').one('click', getOverlayContent)
 }
