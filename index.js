695147aa765d109fa19c45f89a91bb3f7567fc63
cdelauder <cdelauder@gmail.com>
2014-09-23 17:05:18 -0700

    node app with express and express-handlebars. the first route, index is almost up. just need to adjust it to serve the picture.

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express')
+var server = express()
+var exphbs = require('express-handlebars')
+server.engine('handlebars', exphbs({defaultLayout: 'layout'}));
+server.set('view engine', 'handlebars');
+
+server.get('/', function (req, res) {
+  res.render('index')
+})
+
+server.get('/stylesheet.css', function (req, res) {
+  res.sendFile(__dirname + '/public/css/stylesheet.css')
+})
+
+server.listen(3000)
